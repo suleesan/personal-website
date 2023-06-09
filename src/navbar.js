e30d36ebda91ec3331116d09ae06fe87373bc6ef
Susan Lee <109441419+suleesan@users.noreply.github.com>
2023-06-08 23:57:57 -0700

    fixed LinkedIn link

--- src/navbar.js
+++ src/navbar.js
@@ -8,8 +8,8 @@ function Navbar() {
     <header>
       <nav className="z-10 flex w-full flex-row justify-between lg:px-8" aria-label="Global">
         <div className=" inset-0 z-10 bg-transparent w-full flex flex-row justify-between py-4">
-          <div href="#" className="m-1.5 p-1.5 flex flex-row justify-between items-center">
-            <button as="a" href="https://www.linkedin.com/in/susan-lee-12957a217/" target="_blank" rel="noreferrer" className="mr-8"><FiLinkedin size={24}/></button>
+          <div className="m-1.5 p-1.5 flex flex-row justify-between items-center">
+            <a href="https://www.linkedin.com/in/susan-lee-12957a217/" target="_blank" rel="noreferrer" className="mr-8"><FiLinkedin size={24}/></a>
             <button onClick={() => window.location.href = 'mailto:leesusan@stanford.edu'}><FiMail size={24}/></button>
           </div>
           <div className="mt-6 flow-root">
