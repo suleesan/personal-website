ca3c44f5615fa5dc99e30c7575555920180cbbec
Susan Lee <109441419+suleesan@users.noreply.github.com>
2023-08-20 10:18:29 -0700

    Update blog.js

--- src/components/blog.js
+++ src/components/blog.js
@@ -28,7 +28,7 @@ export default function Blog() {
             From the blog
           </h2>
           <p className="mt-2 text-base sm:text-lg leading-8 text-gray-600">
-            Tidbits from my life. Still under construction.
+            Tidbits from my life.
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none">
