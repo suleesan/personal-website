4722a28fdca2a69b07837f5a49d922822c7651c7
Susan Lee <109441419+suleesan@users.noreply.github.com>
2024-12-21 03:44:21 -0800

    update projects
    - render them from latest to earliest
    - update descriptions to be more flexible

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,7 +7,6 @@ import AvoglowSymptoms from "../images/Avoglow Sympoms Page.png";
 import AvoglowCalendar from "../images/Avoglow Calendar Page.png";
 import Spotify from "../images/Spotify Top Tracks Project.png";
 import Twitter from "../images/twitter clone.png";
-import PersonalWebsite from "../images/personal website.png";
 import WordleLight from "../images/Wordle Light.png";
 import WordleDark from "../images/Wordle Dark.png";
 import Pokedex from "../images/pokedex.png";
@@ -84,27 +83,40 @@ export default function Projects() {
   const accordionData = [
     {
       project: "Avoglow",
-      description:
-        "Avoglow is a skincare app inspired by Clue. My friends and I created this app using React Native. Users can record their daily skin symptoms (such as breakouts and dryness), save their logs into a calendar, and access skincare tips. We also created support for user authentication with Supabase.",
+      description: (
+        <p className="text-lg">
+          Avoglow is a skincare app inspired by Clue. My friends and I created
+          this app using React Native. Users can record their daily skin
+          symptoms (such as breakouts and dryness), save their logs into a
+          calendar, and access skincare tips. We also created support for user
+          authentication with Supabase.
+        </p>
+      ),
       images: [AvoglowLogin, AvoglowHome, AvoglowSymptoms, AvoglowCalendar],
     },
     {
       project: "Top Spotify Tracks",
-      description:
-        "Using React Native, I made an app that created a playlist of a user’s top Spotify tracks, utilizing the Spotify API to enable users to connect with their account.",
+      description: (
+        <p className="text-lg">
+          Using React Native, I made an app that created a playlist of a user’s
+          top Spotify tracks, utilizing the Spotify API to enable users to
+          connect with their account.
+        </p>
+      ),
       images: [Spotify],
     },
     {
       project: "Twitter Clone",
-      description:
-        "I created a simple Twitter clone using HTML, CSS, and JavaScript. Using Node.js and Express.js, I implemented a REST API to interact with a MongoDB database to enable users to create a profile, log in, create posts, comment on other users’ posts on their feed, and follow or unfollow other users.",
+      description: (
+        <p className="text-lg">
+          I created a simple Twitter clone using HTML, CSS, and JavaScript.
+          Using Node.js and Express.js, I implemented a REST API to interact
+          with a MongoDB database to enable users to create a profile, log in,
+          create posts, comment on other users’ posts on their feed, and follow
+          or unfollow other users.
+        </p>
+      ),
       images: [Twitter],
-    },
-    {
-      project: "This Website",
-      description:
-        "I designed this website from scratch using Figma and built it using React and styled it with Tailwind. I'm using GraphCMS (Hygraph) for the blog. I've attached an image of what my original Figma design looked like.",
-      images: [PersonalWebsite],
     },
     {
       project: "Wordle",
@@ -112,8 +124,14 @@ export default function Projects() {
         "https://wordle-two-khaki.vercel.app/",
         "https://github.com/suleesan/wordle",
       ],
-      description:
-        "Click the title to try it out! Feeling too impatient to wait every day for a new Wordle, I decided to build my own using React. Features unlimited plays, light/dark mode, and valid guesses and answers according to the actual NYT Wordle. Future improvements: Hard Mode.",
+      description: (
+        <p className="text-lg">
+          Click the title to try it out! Feeling too impatient to wait every day
+          for a new Wordle, I decided to build my own using React. Features
+          unlimited plays, light/dark mode, and valid guesses and answers
+          according to the actual NYT Wordle. Future improvements: Hard Mode.
+        </p>
+      ),
       images: [WordleDark, WordleLight],
     },
     {
@@ -122,8 +140,17 @@ export default function Projects() {
         "https://pokedex-one-cyan.vercel.app/",
         "https://github.com/suleesan/pokedex",
       ],
-      description:
-        "Click the title to play with my Pokedex! I'm currently cooking up a different project up that involves Pokemon, but while I was playing around with the PokeAPI, I thought I'd build a super quick Pokedex with React. It features the first 50 Pokemon in the Pokedex and has a search feature (currently still in the process of figuring out how to load more Pokemon efficiently). I also used Redux and local storage to archive data and reduce load time. Enjoy!",
+      description: (
+        <p className="text-lg">
+          Click the title to play with my Pokedex! I'm currently cooking up a
+          different project that involves Pokemon, but while I was playing
+          around with the PokeAPI, I thought I'd build a super quick Pokedex
+          with React. It features the first 50 Pokemon in the Pokedex and has a
+          search feature (currently still in the process of figuring out how to
+          load more Pokemon efficiently). I also used Redux and local storage to
+          archive data and reduce load time. Enjoy!
+        </p>
+      ),
       images: [Pokedex],
     },
     {
@@ -132,8 +159,18 @@ export default function Projects() {
         "https://devpost.com/software/openplate",
         "https://github.com/collinjung/kindkitchen",
       ],
-      description:
-        "For TreeHacks 2024, my team and I built KindKitchen using React Native and Convex. We aimed to connect patients to cultural, home-cooked meals provided by volunteers and allow users to opt-in to shared meal experiences to combat loneliness and isolation in their treatment journey. Users can specify dietary restrictions and preferences to filter meals, request a meal to be delivered, or alternatively if no meals are available, chat with an AI chatbot that provides easy-to-cook recipes tailored to the user’s restrictions/preferences.",
+      description: (
+        <p className="text-lg">
+          For TreeHacks 2024, my team and I built KindKitchen using React Native
+          and Convex. We aimed to connect patients to cultural, home-cooked
+          meals provided by volunteers and allow users to opt-in to shared meal
+          experiences to combat loneliness and isolation in their treatment
+          journey. Users can specify dietary restrictions and preferences to
+          filter meals, request a meal to be delivered, or alternatively if no
+          meals are available, chat with an AI chatbot that provides
+          easy-to-cook recipes tailored to the user’s restrictions/preferences.
+        </p>
+      ),
       images: [KindKitchen1, KindKitchen2, KindKitchen3, KindKitchen4],
     },
     {
@@ -142,8 +179,25 @@ export default function Projects() {
         "https://course-calendar.vercel.app/",
         "https://github.com/suleesan/course-calendar",
       ],
-      description:
-        "This is a simple course planner built with React where I plan out my course schedules for every quarter. Solutions exist (ex: Carta, onCourse) where it'll automatically plan out quarters for you, but I like the manual aspect. Users can add/delete courses per quarter, switch between quarters, update classes, and this is all saved with local storage. You can also share your calendar! View mine here (https://course-calendar.vercel.app/?id=5ddb9557-6598-4512-bb12-841de10018dc)",
+      description: (
+        <p className="text-lg">
+          This is a simple course planner built with React where I plan out my
+          course schedules for every quarter. Solutions exist (ex: Carta,
+          onCourse) where it'll automatically plan out quarters for you, but I
+          like the manual aspect. Users can add/delete courses per quarter,
+          switch between quarters, and update classes. All data is saved with
+          local storage, and you can share your calendar. View mine{" "}
+          <a
+            href="https://course-calendar.vercel.app/?id=5ddb9557-6598-4512-bb12-841de10018dc"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary-500 hover:bg-gray-100 hover:px-1 rounded-lg"
+          >
+            here
+          </a>
+          !
+        </p>
+      ),
       images: [CourseCalendar],
     },
   ];
@@ -156,69 +210,72 @@ export default function Projects() {
         </h2>
       </div>
       <div id="accordion" className="sm:mx-8">
-        {accordionData.map((item, index) => (
-          <div key={index} className="block relative w-full">
-            <button
-              type="button"
-              className="flex justify-between items-center w-full py-4 border-b border-b-blue-gray-100 text-xl text-left font-semibold leading-snug select-none hover:text-blue-gray-900 transition-colors"
-              onClick={() => handleOpen(index)}
-            >
-              <h2
-                className={`flex flex-row justify-between text-xl sm:text-2xl font-bold ${
-                  item.link ? "hover:text-primary-500" : ""
-                }`}
+        {accordionData
+          .slice()
+          .reverse()
+          .map((item, index) => (
+            <div key={index} className="block relative w-full">
+              <button
+                type="button"
+                className="flex justify-between items-center w-full py-4 border-b border-b-blue-gray-100 text-xl text-left font-semibold leading-snug select-none hover:text-blue-gray-900 transition-colors"
+                onClick={() => handleOpen(index)}
               >
-                {item.link ? (
-                  <a
-                    href={item.link[0]}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-primary-500 rounded-lg hover:px-2 hover:py-1.5 hover:bg-gray-100"
-                  >
-                    {item.project}
-                  </a>
-                ) : (
-                  item.project
-                )}
-              </h2>
-              <Icon id={index} open={openIndexes.includes(index)} />
-            </button>
-            {openIndexes.includes(index) && (
-              <div className="overflow-hidden">
-                <div className="inline-block w-full py-4 px-4 text-gray-700 text-sm font-light leading-normal">
-                  <p className="text-lg">{item.description}</p>
-                  {item.link && (
-                    <p className="mt-2 text-lg">
-                      For the code, click{" "}
-                      <a
-                        href={item.link[1]}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-primary-500 hover:bg-gray-100 hover:px-1 rounded-lg"
-                      >
-                        here
-                      </a>
-                      .
-                    </p>
+                <h2
+                  className={`flex flex-row justify-between text-xl sm:text-2xl font-bold ${
+                    item.link ? "hover:text-primary-500" : ""
+                  }`}
+                >
+                  {item.link ? (
+                    <a
+                      href={item.link[0]}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-primary-500 rounded-lg hover:px-2 hover:py-1.5 hover:bg-gray-100"
+                    >
+                      {item.project}
+                    </a>
+                  ) : (
+                    item.project
                   )}
-                  {item.images && (
-                    <div className="flex flex-col sm:flex-row sm:flex-shrink mt-4">
-                      {item.images.map((image, imageIndex) => (
-                        <div className="w-full max-w-sm mx-auto mt-2 px-2">
-                          <img
-                            key={imageIndex}
-                            src={image}
-                            alt="project images"
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  )}
+                </h2>
+                <Icon id={index} open={openIndexes.includes(index)} />
+              </button>
+              {openIndexes.includes(index) && (
+                <div className="overflow-hidden">
+                  <div className="inline-block w-full py-4 px-4 text-gray-700 text-sm font-light leading-normal">
+                    {item.description}
+                    {item.link && (
+                      <p className="mt-2 text-lg">
+                        For the code, click{" "}
+                        <a
+                          href={item.link[1]}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-primary-500 hover:bg-gray-100 hover:px-1 rounded-lg"
+                        >
+                          here
+                        </a>
+                        .
+                      </p>
+                    )}
+                    {item.images && (
+                      <div className="flex flex-col sm:flex-row sm:flex-shrink mt-4">
+                        {item.images.map((image, imageIndex) => (
+                          <div className="w-full max-w-sm mx-auto mt-2 px-2">
+                            <img
+                              key={imageIndex}
+                              src={image}
+                              alt="project images"
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
+              )}
+            </div>
+          ))}
       </div>
       <div className="mt-12">
         {!isAuthorized ? (
